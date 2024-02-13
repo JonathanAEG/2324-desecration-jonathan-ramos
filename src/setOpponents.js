bfb8918c079e88a3eb80cbaa5bcc0a6df8ff92ed
Jonathan Ramos <jonathan.ramos@ikasle.aeg.eus>
2024-02-13 12:27:26 +0100

    [MODIFY] an object is created with hero data

--- src/setOpponents.js
+++ src/setOpponents.js
@@ -5,13 +5,36 @@ const setSuperHero = (heroes)=>{
 
     const randomHeroIndex = Math.floor(Math.random()*(filteredHeroes.length));
 
-    return filteredHeroes[randomHeroIndex];
+    const randomHero = filteredHeroes[randomHeroIndex];
+    const hero = {
+        name: randomHero.name,
+        intelligence: randomHero.powerstats.intelligence,
+        strength: randomHero.powerstats.strength,
+        speed: randomHero.powerstats.speed,
+        durability: randomHero.powerstats.durability,
+        power: randomHero.powerstats.power,
+        combat: randomHero.powerstats.combat,
+    }
+
+    return hero;
 }
 
 const setVillainZarate = (heroes)=>{
 
     const villain = heroes.filter((hero)=>hero.name === "Junkpile");
-    return villain[0];
+    
+
+    const villainObject = {
+        name: villain[0].name,
+        intelligence: villain[0].powerstats.intelligence,
+        strength: villain[0].powerstats.strength,
+        speed: villain[0].powerstats.speed,
+        durability: villain[0].powerstats.durability,
+        power: villain[0].powerstats.power,
+        combat: villain[0].powerstats.combat,
+    }
+
+    return villainObject;
 }
 
 module.exports = {
