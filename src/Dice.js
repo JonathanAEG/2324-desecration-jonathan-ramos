a38856b260171249906ded24aead67e6e1bf8cde
Jonathan Ramos <jonathan.ramos@ikasle.aeg.eus>
2024-02-13 12:26:26 +0100

    [MODIFY] Modify return value for multiple dice rolls

--- src/Dice.js
+++ src/Dice.js
@@ -15,34 +15,34 @@ module.exports = class Dice {
 
     static dice2D3(){
 
-        const values = [];
+        let value = 0
 
         for(let i = 0; i < 2; i++){
-            values.push(Math.floor(Math.random()*(3)+1))
+            value += Math.floor(Math.random()*(3)+1);
         }
 
-        return values;
+        return value;
     }
 
     static dice4D3(){
 
-        const values = [];
+        let value = 0;
 
         for(let i = 0; i < 4; i++){
-            values.push(Math.floor(Math.random()*(3)+1))
+            value += Math.floor(Math.random()*(3)+1);
         }
         
-        return values;
+        return value;
     }
 
     static dice3D5(){
 
-        const values = [];
+        let value = 0;
 
         for(let i = 0; i < 3; i++){
-            values.push(Math.floor(Math.random()*(5)+1))
+            value += Math.floor(Math.random()*(5)+1);
         }
         
-        return values;
+        return value;
     }
 }
